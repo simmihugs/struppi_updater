66fbcbae65575b01409ee00aa46610d25f8ee0a9
simmihugs <simon.graetz@gmx.de>
2025-01-17 11:35:50 +0100

    refactor2

--- src/xml_reader/reader.rs
+++ src/xml_reader/reader.rs
@@ -11,29 +11,5 @@ pub fn read(path: &str) -> std::io::Result<()> {
     reader.config_mut().trim_text(true);
     
     println!("{:?}", reader);
-
-    /*
-    reader.trim_text(true);
-
-    // Open the output XML file
-    let mut writer = Writer::new(BufWriter::new(File::create(format!("output_{}", path)?)));
-
-    let mut buf = Vec::new();
-
-    // Loop through all events in the input XML
-    loop {
-        match reader.read_event_into(&mut buf) {
-            Ok(Event::Eof) => break, // End of file reached
-            Ok(event) => {
-                // Write the event to the output XML
-                writer.write_event(event)?;
-            }
-            Err(e) => {
-                eprintln!("Error at position {}: {:?}", reader.buffer_position(), e);
-                break;
-            }
-        }
-    }
-    */
     Ok(())
 }
